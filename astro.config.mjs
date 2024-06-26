08da27c8f366d9f02576151f9cf6a07257f4d7e7
Dhairya  Shah <dtshah24022006@gmail.com>
2024-06-26 11:58:41 +0530

    add sanity studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+    projectId: "fw4fh611",
+    dataset: "production",
+    useCdn: false,
+    apiVersion: "2024-06-26",
+    studioBasePath: "/studio"
+  }), react()]
+});
